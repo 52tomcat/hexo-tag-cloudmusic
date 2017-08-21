68d3206f0c5c3950410e6157f7bd7f8fbb3b11a9
asuna <mikoto.yu51@gmail.com>
2017-08-21 14:56:57 +0800

    Fix autoPlay and use html5 as default, auto choose http/https

--- hexo-tag-cloudmusic/index.js
+++ hexo-tag-cloudmusic/index.js
@@ -12,9 +12,9 @@ hexo.extend.tag.register('cloudmusic', function(args){
   //网易云音乐id 
   var sid = args[0];
   var config = hexo.config.cloudmusic || {};
-  var widgetType = hexo.config.widgetType || 'embed';
-  var widgetSize = config.widgetSize || 'small';
-  var autoPlay = config.autoPlay || 1;
+  var widgetType = config.widgetType || 'iframe';
+  var widgetSize = config.widgetSize || 'large';
+  var autoPlay = config.autoPlay.toString() || 1;
 
   if(widgetType == "iframe"){
     return createByIFrame(sid, autoPlay, widgetSize);
@@ -27,17 +27,17 @@ hexo.extend.tag.register('cloudmusic', function(args){
 function crateByEmbed(sid, autoPlay, widgetSize)
 {
    if(widgetSize == 'large'){
-      return '<embed src="http://music.163.com/style/swf/widget.swf?sid=' + sid + '&type=2&auto=' + autoPlay + '&width=320&height=66" width="340" height="86"  allowNetworking="all"></embed>';
+      return '<embed src="//music.163.com/style/swf/widget.swf?sid=' + sid + '&type=2&auto=' + autoPlay + '&width=320&height=66" width="340" height="86"  allowNetworking="all"></embed>';
    }else if(widgetSize == 'small'){
-      return '<embed src="http://music.163.com/style/swf/widget.swf?sid=' + sid + '&type=2&auto=' + autoPlay + '&width=278&height=32" width="298" height="52"  allowNetworking="all"></embed>';
+      return '<embed src="//music.163.com/style/swf/widget.swf?sid=' + sid + '&type=2&auto=' + autoPlay + '&width=278&height=32" width="298" height="52"  allowNetworking="all"></embed>';
    }
 }
 
 function createByIFrame(sid, autoPlay, widgetSize)
 {
   if(widgetSize == 'large'){
-    return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=351 height=86 src="http://music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay + '&height=66"></iframe>';
+    return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=351 height=86 src="//music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay + '&height=66"></iframe>';
   }else if(widgetSize == 'small'){
-    return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="http://music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay +'&height=32"></iframe>';
+    return '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=' + sid + '&auto=' + autoPlay +'&height=32"></iframe>';
   }
 }
